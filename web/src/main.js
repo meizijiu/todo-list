ac8ebd09cdbeb483aeef10467cc49808db71e045
mzwine <i@mzwine.cc>
2023-05-04 12:50:49 +0800

    配置vercel Analytics

--- web/src/main.js
+++ web/src/main.js
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { createAnalytics } from '@vercel/analytics'
 import App from './App.vue'
 
 const app = createApp(App)
+
+const analytics = createAnalytics('prj_hT12ELDAXFatrkIBCMT4rMd8a2nE')
+
+analytics.page()
 
 app.use(createPinia()).use(ElementPlus).mount('#app')
